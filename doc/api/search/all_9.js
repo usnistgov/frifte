3eb0c3d2d56016205c3efc7bf4edd1b56c06e8fe
Greg Fiumara <gregory.fiumara@nist.gov>
2025-02-20 14:59:26 -0500

    Removal of TemplateData's imageQuality

--- doc/api/search/all_9.js
+++ doc/api/search/all_9.js
@@ -3,12 +3,11 @@ var searchData=
   ['identifier_0',['identifier',['../struct_f_r_i_f_1_1_image.html#af7fb5c63be65510bc4a2bb7ffa288fad',1,'FRIF::Image::identifier'],['../struct_f_r_i_f_1_1_e_f_s_1_1_quality_measure_1_1_description.html#a11a8c23736debd0168785d4908917e72',1,'FRIF::EFS::QualityMeasure::Description::identifier'],['../struct_f_r_i_f_1_1_e_f_s_1_1_features.html#add5cc048ec5623c6da9e2611b7b8b519',1,'FRIF::EFS::Features::identifier'],['../struct_f_r_i_f_1_1_evaluations_1_1_library_identifier.html#a41da51afda1013f9c4ecb1f05b2c079a',1,'FRIF::Evaluations::LibraryIdentifier::identifier'],['../struct_f_r_i_f_1_1_template_data.html#a89a12e30dcca4a5652e6ac5067436285',1,'FRIF::TemplateData::identifier'],['../struct_f_r_i_f_1_1_subject_position_candidate.html#ad26507bfa0fe09126c0054b91755af44',1,'FRIF::SubjectPositionCandidate::identifier']]],
   ['image_1',['Image',['../struct_f_r_i_f_1_1_image.html',1,'FRIF::Image'],['../struct_f_r_i_f_1_1_image.html#a9bd52b898c5b004096228ca338b03e73',1,'FRIF::Image::Image()'],['../struct_f_r_i_f_1_1_image.html#ab13e34c36aa4cea3c3138e099f30d147',1,'FRIF::Image::Image(const uint8_t identifier, const uint16_t width, const uint16_t height, const uint16_t ppi, const Colorspace colorspace, const BitsPerChannel bpc, const BitsPerPixel bpp, const std::vector&lt; std::byte &gt; &amp;pixels)']]],
   ['image_20and_20features_20technology_20evaluations_2',['Friction Ridge Image and Features Technology Evaluations',['../index.html',1,'']]],
-  ['imagequality_3',['imageQuality',['../struct_f_r_i_f_1_1_template_data.html#afe74e49c30ef1b518318a1f3626c539e',1,'FRIF::TemplateData']]],
-  ['imp_4',['IMP',['../namespace_f_r_i_f_1_1_e_f_s.html#a305408e887d8d31584367c6dcc0e8b4b',1,'FRIF::EFS']]],
-  ['imp_5',['imp',['../struct_f_r_i_f_1_1_e_f_s_1_1_features.html#a88da981b824e3972d5ef68d3bbfe205d',1,'FRIF::EFS::Features']]],
-  ['impression_6',['Impression',['../namespace_f_r_i_f_1_1_e_f_s.html#aaac3a13e08279640e83afe55225ead4b',1,'FRIF::EFS']]],
-  ['indanedione_7',['Indanedione',['../namespace_f_r_i_f_1_1_e_f_s.html#a84d409195776b926d7a72a5de326a89ba54f1c68277eae7776f35b64f700ac82e',1,'FRIF::EFS']]],
-  ['inner_8',['Inner',['../namespace_f_r_i_f_1_1_e_f_s.html#af744aac6be216e7fd06ce24c8dc84aa0aa1684698ac3d8ffda353d0458c30b1e7',1,'FRIF::EFS']]],
-  ['inputidentifier_9',['inputIdentifier',['../struct_f_r_i_f_1_1_template_data.html#ae7d86809aa4712489b41237578965717',1,'FRIF::TemplateData']]],
-  ['io_2eh_10',['io.h',['../io_8h.html',1,'']]]
+  ['imp_3',['IMP',['../namespace_f_r_i_f_1_1_e_f_s.html#a305408e887d8d31584367c6dcc0e8b4b',1,'FRIF::EFS']]],
+  ['imp_4',['imp',['../struct_f_r_i_f_1_1_e_f_s_1_1_features.html#a88da981b824e3972d5ef68d3bbfe205d',1,'FRIF::EFS::Features']]],
+  ['impression_5',['Impression',['../namespace_f_r_i_f_1_1_e_f_s.html#aaac3a13e08279640e83afe55225ead4b',1,'FRIF::EFS']]],
+  ['indanedione_6',['Indanedione',['../namespace_f_r_i_f_1_1_e_f_s.html#a84d409195776b926d7a72a5de326a89ba54f1c68277eae7776f35b64f700ac82e',1,'FRIF::EFS']]],
+  ['inner_7',['Inner',['../namespace_f_r_i_f_1_1_e_f_s.html#af744aac6be216e7fd06ce24c8dc84aa0aa1684698ac3d8ffda353d0458c30b1e7',1,'FRIF::EFS']]],
+  ['inputidentifier_8',['inputIdentifier',['../struct_f_r_i_f_1_1_template_data.html#ae7d86809aa4712489b41237578965717',1,'FRIF::TemplateData']]],
+  ['io_2eh_9',['io.h',['../io_8h.html',1,'']]]
 ];
--- doc/api/search/variables_7.js
+++ doc/api/search/variables_7.js
@@ -1,7 +1,6 @@
 var searchData=
 [
   ['identifier_0',['identifier',['../struct_f_r_i_f_1_1_image.html#af7fb5c63be65510bc4a2bb7ffa288fad',1,'FRIF::Image::identifier'],['../struct_f_r_i_f_1_1_e_f_s_1_1_quality_measure_1_1_description.html#a11a8c23736debd0168785d4908917e72',1,'FRIF::EFS::QualityMeasure::Description::identifier'],['../struct_f_r_i_f_1_1_e_f_s_1_1_features.html#add5cc048ec5623c6da9e2611b7b8b519',1,'FRIF::EFS::Features::identifier'],['../struct_f_r_i_f_1_1_evaluations_1_1_library_identifier.html#a41da51afda1013f9c4ecb1f05b2c079a',1,'FRIF::Evaluations::LibraryIdentifier::identifier'],['../struct_f_r_i_f_1_1_template_data.html#a89a12e30dcca4a5652e6ac5067436285',1,'FRIF::TemplateData::identifier'],['../struct_f_r_i_f_1_1_subject_position_candidate.html#ad26507bfa0fe09126c0054b91755af44',1,'FRIF::SubjectPositionCandidate::identifier']]],
-  ['imagequality_1',['imageQuality',['../struct_f_r_i_f_1_1_template_data.html#afe74e49c30ef1b518318a1f3626c539e',1,'FRIF::TemplateData']]],
-  ['imp_2',['imp',['../struct_f_r_i_f_1_1_e_f_s_1_1_features.html#a88da981b824e3972d5ef68d3bbfe205d',1,'FRIF::EFS::Features']]],
-  ['inputidentifier_3',['inputIdentifier',['../struct_f_r_i_f_1_1_template_data.html#ae7d86809aa4712489b41237578965717',1,'FRIF::TemplateData']]]
+  ['imp_1',['imp',['../struct_f_r_i_f_1_1_e_f_s_1_1_features.html#a88da981b824e3972d5ef68d3bbfe205d',1,'FRIF::EFS::Features']]],
+  ['inputidentifier_2',['inputIdentifier',['../struct_f_r_i_f_1_1_template_data.html#ae7d86809aa4712489b41237578965717',1,'FRIF::TemplateData']]]
 ];
